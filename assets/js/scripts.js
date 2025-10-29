a663f963d574cef7a5f4a5d8691ef79af448b753
Marcos Vinicius <marcosamaralwebdeveloper@outlook.com>
2025-10-29 20:37:46 -0300

    Ajustes de refinamento

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,54 +1,93 @@
-const titleElement = document.querySelector(".hero-title");
-const originalText = titleElement.textContent;
-const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+=-[]{}\\|:;\"',.<>/?";
+document.addEventListener("DOMContentLoaded", function () {
 
-function generateRandomChar() {
-  return chars[Math.floor(Math.random() * chars.length)];
-}
+  const toggleButton = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+  const mainNav = document.querySelector(".main-nav");
 
-function glitchText(element, originalText) {
-  const textLength = originalText.length;
-  let glitchCount = 0;
+  if (toggleButton && navLinks && mainNav) {
+    toggleButton.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+      mainNav.classList.toggle("menu-open");
 
-  const glitchInterval = setInterval(() => {
-    let newText = "";
-    const glitchDuration = 4;
-
-    if (glitchCount < glitchDuration) {
-      for (let i = 0; i < textLength; i++) {
-        if (Math.random() < 0.3) {
-          newText += generateRandomChar();
-        } else {
-          newText += originalText[i];
-        }
+      const icon = toggleButton.querySelector("i");
+      if (icon) {
+        icon.classList.toggle("fa-bars");
+        icon.classList.toggle("fa-times");
       }
+    });
 
-      glitchCount++;
-    } else {
-      newText = originalText;
-      clearInterval(glitchInterval);
+    navLinks.querySelectorAll("a").forEach((link) => {
+      link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+        mainNav.classList.remove("menu-open");
+        const icon = toggleButton.querySelector("i");
+        if (icon) {
+          icon.classList.remove("fa-times");
+          icon.classList.add("fa-bars");
+        }
+      });
+    });
+  }
+
+  const titleElement = document.querySelector(".hero-title");
+
+  if (titleElement) {
+    const originalText = titleElement.textContent;
+    const chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+=-[]{}\\|:;\"',.<>/?";
+
+    function generateRandomChar() {
+      return chars[Math.floor(Math.random() * chars.length)];
     }
 
-    element.textContent = newText;
-  }, 50);
-}
+    function glitchText(element, originalText) {
+      const textLength = originalText.length;
+      let glitchCount = 0;
 
-function startGlitchLoop() {
-  glitchText(titleElement, originalText);
+      const glitchInterval = setInterval(() => {
+        let newText = "";
+        const glitchDuration = 4;
 
-  setInterval(() => {
-    glitchText(titleElement, originalText);
-  }, 7000);
-}
+        if (glitchCount < glitchDuration) {
+          for (let i = 0; i < textLength; i++) {
+            if (Math.random() < 0.4) {
+              newText += generateRandomChar();
+            } else {
+              newText += originalText[i];
+            }
+          }
+
+          glitchCount++;
+        } else {
+          newText = originalText;
+          clearInterval(glitchInterval);
+        }
+
+        element.textContent = newText;
+      }, 50);
+    }
+
+    function startGlitchLoop() {
+      glitchText(titleElement, originalText);
+
+      setInterval(() => {
+        glitchText(titleElement, originalText);
+      }, 7000);
+    }
+
+    startGlitchLoop();
+  }
+});
 
 function handleScroll() {
   const nav = document.querySelector(".main-nav");
-  if (window.scrollY > 100) {
-    nav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
+  if (nav) {
+    if (window.scrollY > 100) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
   }
 }
 
-window.addEventListener("load", startGlitchLoop);
 window.addEventListener("scroll", handleScroll);
